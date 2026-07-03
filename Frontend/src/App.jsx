38d6d5fd445deca865579aee94a09f96321730fb
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-07-03 14:48:55 -0300

    Carpeta Frontend & Estructura inicial ReactJs

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from './routes';
+import './App.css';
+
+function App() {
+
+	return (
+		<div className='app_container'>
+			<AppRoutes />
+		</div>
+	);
+}
+
+export default App;
